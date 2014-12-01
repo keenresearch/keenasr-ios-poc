a0eab67a3b89289cf1322c5dbcda5ce070f53c02
Ognjen Todic <ogi@keenresearch.com>
2014-12-01 14:05:11 -0800

    Initial commit

---
+++ kaldi-ios-poc/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  kaldi-ios-poc
+//
+//  Created by Ognjen Todic on 11/14/14.
+//  Copyright (c) 2014 Keen Research. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ kaldi-ios-poc/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  kaldi-ios-poc
+//
+//  Created by Ognjen Todic on 11/14/14.
+//  Copyright (c) 2014 Keen Research. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
