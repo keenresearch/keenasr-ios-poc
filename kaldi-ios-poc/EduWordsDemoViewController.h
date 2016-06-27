ef6f9ecefda573ac709c199fc3931bc91c9bfed8
Ognjen Todic <ogi@keenresearch.com>
2016-06-27 16:37:11 -0700

    update comments

--- kaldi-ios-poc/EduWordsDemoViewController.h
+++ kaldi-ios-poc/EduWordsDemoViewController.h
@@ -5,7 +5,7 @@
 //  Created by Ognjen Todic on 6/27/16.
 //  Copyright © 2016 Keen Research. All rights reserved.
 //
-// In this demo, 1000 most frequent words used by kids are used to create a
+// In this demo, ~1000 most frequent words used by children are used to create a
 // decoding graph. The app helps kids learn spelling of those words, by letting
 // them say the word ("spell mouse", "how do you spell mouse", "mouse"). See
 // the controller source code for details on how partial results are used for
